fa2ef6ec5b6d38a8deef7a60f290487c79857bea
Leonardo Yojo <l_yojo@hotmail.com>
2022-04-27 05:16:31 -0300

    Segunda versão

--- tweet_test.py
+++ tweet_test.py
@@ -2,10 +2,15 @@ import tweepy
 import pandas as pd
 import datetime as dt
 import re
+import csv
 from nltk.corpus import stopwords
+from nltk.metrics import distance
 from collections import Counter
 
 def remove_emojis(texto):
+    """ 
+    Função para remover emotes de uma string
+    """
     emoj = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -28,9 +33,30 @@ def remove_emojis(texto):
                       "]+", re.UNICODE)
     return re.sub(emoj, '', texto)
 
-# Escolhe o dia em que vamos pegar os tweets
-now = dt.datetime.now()
-delta = now - dt.timedelta(days = 6)
+def ranker(word_list, tweet_text, tweet_id):
+    """
+    Função para rankear o texto de um tweet, dado um dicionário de palavras e pontos
+    """
+    pontuacao = 0
+    palavras = list(set(word_list.keys()).intersection(set(tweet_text.split(" "))))
+    #print(palavras)
+    for word in palavras:
+        pontuacao += word_list[word]
+    result = {"pontuacao":pontuacao, "palavras":palavras, "id":int(tweet_id)}
+    return result
+
+def e_repetido(lista_tweets, texto):
+    """
+    Função para ver a probabilidade de texto ser repetido dentre os conteúdos nos tweets da lista_tweets
+    """
+    if len(lista_tweets) > 0:
+        max_pontuacao = 0
+        for itens in lista_tweets:
+            pont_temp = distance.jaro_similarity(itens["text"], texto)
+            if pont_temp > max_pontuacao:
+                max_pontuacao = pont_temp
+        return max_pontuacao
+    return 0
 
 pd.set_option("display.max_columns", None)
 
@@ -38,34 +64,50 @@ pd.set_option("display.max_columns", None)
 bearer_token = ""
 
 # Campos dos tweets que vamos pegar
-fields = ["id","text","author_id","created_at","geo","lang","public_metrics","source"]
+fields = ["id","text","author_id","created_at","lang","source","public_metrics"]
+fields_df = ["id","text","author_id","created_at","lang","source","retweet_count","reply_count","like_count","quote_count"]
 
 client = tweepy.Client(bearer_token)
 
 # Defini a query que vamos buscar
-assunto = "Will Smith lang:en"
+palavra = "sport"
+query = palavra + " -is:retweet -is:reply lang:en"
 
-response = client.search_recent_tweets(assunto, tweet_fields=fields, max_results=100, end_time=delta, sort_order="relevancy")
-tweets = response.data
+# Faz um loop para para fazer vários requests de tweets com diferentes horários
+for time_dif in range(10):
+    # Escolhe o dia em que vamos pegar os tweets
+    delta = dt.datetime.now() - dt.timedelta(days = 6, hours = time_dif)
 
-# Cria uma lista de dicionários com os tweets
-lista = []
-for tweet in tweets:
-    dic_aux={"id": tweet["id"],
-    "text": tweet["text"],
-    "author_id": tweet["author_id"],
-    "created_at": tweet["created_at"],
-    "lang": tweet["lang"],
-    "source": tweet["source"],
-    "retweet_count": tweet["public_metrics"]["retweet_count"],
-    "reply_count": tweet["public_metrics"]["reply_count"],
-    "like_count": tweet["public_metrics"]["like_count"],
-    "quote_count": tweet["public_metrics"]["quote_count"]
-    }
-    lista.append(dic_aux)
+    # Pega os tweets
+    response = client.search_recent_tweets(query, tweet_fields=fields, max_results=100, end_time=delta, sort_order="relevancy")
+    tweets = response.data
+
+    # Cria uma lista de dicionários com os tweets, caso o texto do tweet não seja repetido
+    lista = []
+    for tweet in tweets:
+        if e_repetido(lista, tweet.text) < 0.9:
+            dic_aux={"id": tweet["id"],
+            "text": tweet["text"],
+            "author_id": tweet["author_id"],
+            "created_at": tweet["created_at"],
+            "lang": tweet["lang"],
+            "source": tweet["source"],
+            "retweet_count": tweet["public_metrics"]["retweet_count"],
+            "reply_count": tweet["public_metrics"]["reply_count"],
+            "like_count": tweet["public_metrics"]["like_count"],
+            "quote_count": tweet["public_metrics"]["quote_count"]
+            }
+            lista.append(dic_aux)
+
+    # Salva a lista de tweets em um arquivo
+    with open("tweets.csv", "a", encoding="utf-8") as csvfile:
+        csvfile.write("\n")
+        dict_writer = csv.DictWriter(csvfile, fields_df)
+        dict_writer.writerows(lista)
 
 # Cria um DataFrame com os dados dos tweets
-df = pd.DataFrame.from_dict(lista)
+#df = pd.DataFrame.from_dict(lista)
+df = pd.read_csv("tweets.csv")
 
 # Armazena todos os textos dos tweets em uma variável, e dá uma limpada nas palavras
 text_complete = ""
@@ -73,14 +115,14 @@ for i in range(len(df["text"])):
     text_complete += df["text"][i] + " "
 text_complete = re.sub(r"http\S+", "", text_complete)
 text_complete = re.sub(r"\n", "", text_complete)
-text_complete = re.sub(r"[,.!?+-;#@$%&|:()]+", "", text_complete)
+text_complete = re.sub(r"[,.!?+-;#@$%&–|:_()'\[\]\}\{}]+", "", text_complete)
 text_complete = re.sub(r"[1-9]+", "", text_complete)
 text_complete = remove_emojis(text_complete)
+text_complete = text_complete.lower()
 
 # Cria uma lista com as palavras separadas e remove as stopwords
 words_to_remove = stopwords.words("english")
-words_to_remove.append("Will")
-words_to_remove.append("Smith")
+words_to_remove.append(palavra)
 filtered_text = [word for word in text_complete.split() if word not in words_to_remove]
 
 # Cria um dicionário com a palavra e o número de vezes que a palavra apareceu nos tweets
@@ -90,8 +132,17 @@ for key, value in word_count.items():
     if value > 1:
         relevant_words.update({key:value})
 
-# Ordena as palavras por ordem das que mais apareceram nos tweets
+# Ordena as palavras por ordem das que mais apareceram nos tweets e printa a lista dessas palavras
 relevant_words = dict(sorted(relevant_words.items(), key=lambda item: item[1], reverse=True))
-
 print(relevant_words)
 
+# Aplica a função de ranqueamento na coluna de texto dos tweets
+dict_aux = df.apply(lambda x: ranker(relevant_words, x["text"], x["id"]), axis=1)
+
+# Junta o resultado do ranqueamento dos tweets no dataframe do tweets
+df_final = df.merge(pd.DataFrame.from_dict(list(dict_aux)), on="id")
+df_final = df_final.drop_duplicates(subset="id")
+
+# Printa os tweets em ordem de maior pontuação e salva em um arquivo
+print(df_final.sort_values("pontuacao", ascending=False).head(10))
+df_final.to_csv("resultado.csv")
